91bf7b8115b8f76bda852fa86d9cbbdc64cf80f5
GaneshHC <121179037+GaneshHC@users.noreply.github.com>
2023-10-20 05:56:35 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+class ButtonClicker extends React.Component {
+  handleClick = () => {
+    alert('Button Clicked');
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click me</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default ButtonClicker;
